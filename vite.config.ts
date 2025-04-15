bf2fb67c43bd570e5983dbe611d82ff389311453
hjp-h <916269026@qq.com>
2025-04-15 21:34:27 +0800

    feat: 支持打包dts文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ tsconfigPath: "./tsconfig.app.json" })],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
